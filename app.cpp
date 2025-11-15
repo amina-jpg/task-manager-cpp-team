bbc6268597ec2e20aad82c70b10a6e51b7acad78
70154408-tech-eman <70154408@student.uol.edu.pk>
2025-11-15 18:33:42 +0500

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -17,38 +17,26 @@ void showAll() {
     }
     cout << "-------------------\n";
 }
+
 void addTask(string title){
-tasks.push_back({title,false});
-cout<< "Added: " << title <<endl;
+    tasks.push_back({title,false});
+    cout<< "Added: " << title <<endl;
 }
-void menu() {
-    while (true) {
-        cout << "\n1=Add 2=View 3=Delete 4=Update 5=Search 6=Mark 7=Save 8=Load 0=Exit\n> ";
-        int choice;
-        cin >> choice;
-        if (choice == 1) {
-            cout << "Enter title: ";
-            string t; cin.ignore(); getline(cin, t);
-            addTask(t);
-        } else if (choice == 2) viewTasks();
-        else if (choice == 3) {
-            int i; cout << "Index: "; cin >> i; deleteTask(i);
-        } else if (choice == 4) {
-            int i; string nt;
-            cout << "Index: "; cin >> i;
-            cin.ignore(); cout << "New title: "; getline(cin, nt);
-            updateTask(i, nt);
-        } else if (choice == 5) {
-            string k; cout << "Keyword: "; cin >> k;
-            searchTask(k);
-        } else if (choice == 6) {
-            int i; cout << "Index: "; cin >> i;
-            markComplete(i, true);
-        } else if (choice == 7) saveTasks();
-        else if (choice == 8) loadTasks();
-        else if (choice == 0) break;
-        else cout << "Invalid choice!\n";
+
+void viewTasks() {
+    cout << "Displaying all tasks:" << endl;
+    showAll();
+}
+
+void updateTask(int index, string newTitle) {
+    if (index >= 1 && index <= tasks.size()) {
+        tasks[index - 1].title = newTitle;
+        cout << "Task updated to: " << newTitle << endl;
+    } else {
+        cout << "Invalid index" << endl;
     }
+}
+
 void searchTask(string keyword) {
     cout << "Search results for: " << keyword << endl;
     bool found = false;
@@ -60,28 +48,40 @@ void searchTask(string keyword) {
     }
     if (!found) cout << "No matches found." << endl;
 }
-    // Added by B
-void viewTasks() {
-    cout << "Displaying all tasks:" << endl;
-    showAll();
-}
-    // Added by D
-void updateTask(int index, string newTitle) {
-    if (index >= 1 && index <= tasks.size()) {
-        tasks[index - 1].title = newTitle;
-        cout << "Task updated to: " << newTitle << endl;
-    } else {
-        cout << "Invalid index" << endl;
+
+void menu() {
+    while (true) {
+        cout << "\n1=Add 2=View 3=Update 4=Search 0=Exit\n> ";
+        int choice;
+        cin >> choice;
+        if (choice == 1) {
+            cout << "Enter title: ";
+            string t; 
+            cin.ignore(); 
+            getline(cin, t);
+            addTask(t);
+        } 
+        else if (choice == 2) viewTasks();
+        else if (choice == 3) {
+            int i; 
+            string nt;
+            cout << "Index: "; cin >> i;
+            cin.ignore(); 
+            cout << "New title: "; getline(cin, nt);
+            updateTask(i, nt);
+        } 
+        else if (choice == 4) {
+            string k; 
+            cout << "Keyword: "; cin >> k;
+            searchTask(k);
+        } 
+        else if (choice == 0) break;
+        else cout << "Invalid choice!\n";
     }
-}
-
-
-
 }
 
 int main() {
     cout << "Task Manager (Team Project)" << endl;
-    showAll();
     cout << "Welcome to Team Task Manager!\n";
     menu();
     return 0;
