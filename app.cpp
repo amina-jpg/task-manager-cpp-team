0acdde5957d5008b9267ee5530be6e333bf51c6f
70154453-maker <70154453@student.uol.edu.pk>
2025-11-17 21:40:05 +0500

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -90,7 +90,9 @@ void menu() {
     }
 }
 
-int main() {
+int main() 
+//added by taymia
+{
     cout << "Task Manager (Team Project)" << endl;
     cout << "Welcome to Team Task Manager!\n";
     menu();
